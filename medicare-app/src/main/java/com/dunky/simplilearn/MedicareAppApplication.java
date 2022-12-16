a35a7831fdcb8ff830f296cb15afe8cf63dbd564
dunky-star <dukygeoffrey39@gmail.com>
2022-12-16 11:59:33 +0300

    Sprint 1: First milestone - add backend starter files

---
+++ medicare-app/src/main/java/com/dunky/simplilearn/MedicareAppApplication.java
@@ -0,0 +1,13 @@
+package com.dunky.simplilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicareAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicareAppApplication.class, args);
+	}
+
+}
---
+++ medicare-app/src/test/java/com/dunky/simplilearn/MedicareAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dunky.simplilearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicareAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
