e6a0ecda64bd3154566bc5e81ad83554e67f4ad9
dunky-star <dukygeoffrey39@gmail.com>
2022-12-16 16:58:38 +0300

    create REST APIs with Spring Data JPA Repositories and Spring Data REST

--- medicare-app/src/main/java/com/dunky/simplilearn/repositories/DrugCategoryRepository.java
+++ medicare-app/src/main/java/com/dunky/simplilearn/repositories/DrugCategoryRepository.java
@@ -2,6 +2,8 @@ package com.dunky.simplilearn.repositories;
 
 import com.dunky.simplilearn.entity.DrugCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "drugCategory", path = "drug-category")
 public interface DrugCategoryRepository extends JpaRepository<DrugCategory, Long> {
 }
