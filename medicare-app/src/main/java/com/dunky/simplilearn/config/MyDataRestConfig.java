f1eb25b50df9c162ea82c059c940ceef4e068410
dunky-star <dukygeoffrey39@gmail.com>
2022-12-16 20:52:48 +0300

    Implement RepositoryRestConfigurer to disable POST, PUT, DELETE

--- medicare-app/src/main/java/com/dunky/simplilearn/config/MyDataRestConfig.java
+++ medicare-app/src/main/java/com/dunky/simplilearn/config/MyDataRestConfig.java
@@ -1,6 +1,29 @@
 package com.dunky.simplilearn.config;
 
+import com.dunky.simplilearn.entity.Drug;
+import com.dunky.simplilearn.entity.DrugCategory;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
+
+        // disable HTTP methods for Product: PUT, POST, DELETE and PATCH
+        config.getExposureConfiguration()
+                .forDomainType(Drug.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // disable HTTP methods for ProductCategory: PUT, POST, DELETE and PATCH
+        config.getExposureConfiguration()
+                .forDomainType(DrugCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
 }
