0e9cab514bfdd2cdfb34c02ebcc79898e831accd
dunky-star <dukygeoffrey39@gmail.com>
2022-12-16 20:58:01 +0300

    Implement RepositoryRestConfigurer to disable POST, PUT, DELETE

--- medicare-app/src/main/java/com/dunky/simplilearn/config/MyDataRestConfig.java
+++ medicare-app/src/main/java/com/dunky/simplilearn/config/MyDataRestConfig.java
@@ -2,11 +2,13 @@ package com.dunky.simplilearn.config;
 
 import com.dunky.simplilearn.entity.Drug;
 import com.dunky.simplilearn.entity.DrugCategory;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+@Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -14,13 +16,13 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
 
-        // disable HTTP methods for Product: PUT, POST, DELETE and PATCH
+        // disable HTTP methods for Drug: PUT, POST, DELETE and PATCH
         config.getExposureConfiguration()
                 .forDomainType(Drug.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
 
-        // disable HTTP methods for ProductCategory: PUT, POST, DELETE and PATCH
+        // disable HTTP methods for DrugCategory: PUT, POST, DELETE and PATCH
         config.getExposureConfiguration()
                 .forDomainType(DrugCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
