0a92c7ba33a4ec2ddf0d90201b094277fb6625aa
dunky-star <dukygeoffrey39@gmail.com>
2022-12-17 17:19:36 +0300

    CORS policy added to allow Angular service access Spring Boot REST API end-point

--- medicare-app/src/main/java/com/dunky/simplilearn/repositories/DrugRespository.java
+++ medicare-app/src/main/java/com/dunky/simplilearn/repositories/DrugRespository.java
@@ -2,6 +2,7 @@ package com.dunky.simplilearn.repositories;
 
 import com.dunky.simplilearn.entity.Drug;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
+@CrossOrigin("http://localhost:4200")
 public interface DrugRespository extends JpaRepository<Drug, Long> {
 }
