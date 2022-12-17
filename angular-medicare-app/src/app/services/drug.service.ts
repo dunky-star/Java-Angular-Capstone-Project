38c4d5aad37d00282555ea35ceba68910b9cc996
dunky-star <dukygeoffrey39@gmail.com>
2022-12-17 16:10:20 +0300

    Create ng service for HTTP API operations

--- angular-medicare-app/src/app/services/drug.service.ts
+++ angular-medicare-app/src/app/services/drug.service.ts
@@ -1,9 +1,30 @@
+import { Drug } from './../common/drug';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DrugService {
 
-  constructor() { }
+  // API call to the backend REST
+  private baseUrl = 'http://localhost:8585/api/drugs';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getDrugList(): Observable<Drug[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+    map(response => response._embedded.drugs)
+    );
+  }
+
+}
+
+
+interface GetResponse {
+  _embedded: {
+    drugs: Drug[];
+  }
 }
