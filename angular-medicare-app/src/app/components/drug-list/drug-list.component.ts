603579e8bbf07ea2b06732ceb36c24b0c6e5faee
dunky-star <dukygeoffrey39@gmail.com>
2022-12-17 16:27:50 +0300

    Testing - displaying on the Drug List Component HTML page.

--- angular-medicare-app/src/app/components/drug-list/drug-list.component.ts
+++ angular-medicare-app/src/app/components/drug-list/drug-list.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { DrugService } from './../../services/drug.service';
+import { Component, OnInit } from '@angular/core';
+import { Drug } from 'src/app/common/drug';
 
 @Component({
   selector: 'app-drug-list',
   templateUrl: './drug-list.component.html',
   styleUrls: ['./drug-list.component.css']
 })
-export class DrugListComponent {
+export class DrugListComponent implements OnInit{
+  // An array of Drug.
+  drugs: Drug[] = [];
+  // Perform dependency injection of DrugService here so that Drug component can use the REST API.
+  constructor(private drugService: DrugService){
+
+  }
+
+  ngOnInit(): void {
+    this.listDrugs();
+  }
+  listDrugs() {
+    this.drugService.getDrugList().subscribe(
+      data => {
+        this.drugs = data;
+      }
+    )
+  }
 
 }
