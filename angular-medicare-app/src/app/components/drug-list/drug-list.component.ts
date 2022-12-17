7c2c3be63983c34593579d2c0e23bd29d30957ce
dunky-star <dukygeoffrey39@gmail.com>
2022-12-17 14:10:47 +0300

    Add favicon [Me]

---
+++ angular-medicare-app/src/app/components/drug-list/drug-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drug-list',
+  templateUrl: './drug-list.component.html',
+  styleUrls: ['./drug-list.component.css']
+})
+export class DrugListComponent {
+
+}
