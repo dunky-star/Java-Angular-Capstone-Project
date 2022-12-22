71927d4b727b1e38c269018415fc8d2e11fa2f93
dunky-star <dukygeoffrey39@gmail.com>
2022-12-22 11:47:14 +0300

    Checkout Form - Add review your order

--- angular-medicare-app/src/app/components/checkout/checkout.component.ts
+++ angular-medicare-app/src/app/components/checkout/checkout.component.ts
@@ -64,7 +64,7 @@ export class CheckoutComponent implements OnInit {
 
   onSubmit() {
 
-  //console.log("Handling the submit button");
+  console.log("Handling the submit button");
   console.log(this.checkoutFormGroup.get('customer')?.value);
   console.log("The email address is " + this.checkoutFormGroup.get('customer')?.value.email);
   }
