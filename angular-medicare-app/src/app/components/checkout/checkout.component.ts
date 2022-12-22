4581ee9ae04efcbf06942ee30584d06ec22f07b3
dunky-star <dukygeoffrey39@gmail.com>
2022-12-22 11:19:54 +0300

    Change noPropertyAccessFromIndexSignature to false for Angular 15 ReactiveForms

--- angular-medicare-app/src/app/components/checkout/checkout.component.ts
+++ angular-medicare-app/src/app/components/checkout/checkout.component.ts
@@ -18,7 +18,7 @@ export class CheckoutComponent implements OnInit {
   ngOnInit(): void {
 
     this.checkoutFormGroup = this.formBuilder.group({
-      customer: this.formBuilder.group({
+        customer: this.formBuilder.group({
         firstName: [''],
         lastName: [''],
         email: ['']
@@ -48,5 +48,25 @@ export class CheckoutComponent implements OnInit {
     });
   }
 
+  copyShippingAddressToBillingAddress(event: any) {
+
+    if (event.target.checked) {
+      this.checkoutFormGroup.controls.billingAddress
+            .setValue(this.checkoutFormGroup?.controls.shippingAddress.value);
+    }
+    else {
+      this.checkoutFormGroup.controls.billingAddress.reset();
+    }
+
+
+}
+
+
+  onSubmit() {
+
+  console.log("Handling the submit button");
+  console.log(this.checkoutFormGroup.get('customer')?.value);
+  console.log("The email address is " + this.checkoutFormGroup.get('customer')?.value.email);
+  }
 
 }
