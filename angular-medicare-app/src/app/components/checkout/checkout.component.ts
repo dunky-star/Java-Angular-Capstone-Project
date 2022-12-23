a75502f84378a0911e45b8c2c8a5feae638a3b91
dunky-star <dukygeoffrey39@gmail.com>
2022-12-23 16:35:41 +0300

    Checkout form - add custom validation to remove white space bug

--- angular-medicare-app/src/app/components/checkout/checkout.component.ts
+++ angular-medicare-app/src/app/components/checkout/checkout.component.ts
@@ -121,9 +121,14 @@ export class CheckoutComponent implements OnInit {
 
   onSubmit() {
 
-  console.log("Handling the submit button");
-  console.log(this.checkoutFormGroup.get('customer')?.value);
-  console.log("The email address is " + this.checkoutFormGroup.get('customer')?.value.email);
+    // Form validation on submit button click.
+    if (this.checkoutFormGroup.invalid) {
+      this.checkoutFormGroup.markAllAsTouched();
+    }
+
+    console.log("Handling the submit button");
+    console.log(this.checkoutFormGroup.get('customer')?.value);
+    console.log("The email address is " + this.checkoutFormGroup.get('customer')?.value.email);
   }
 
   handleMonthsAndYears() {
