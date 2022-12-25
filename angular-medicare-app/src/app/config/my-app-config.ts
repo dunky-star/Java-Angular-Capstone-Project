454550291939f87fedd026a0b4add96eb269a769
dunky-star <dukygeoffrey39@gmail.com>
2022-12-25 17:23:03 +0300

    Add support for Okta signin widget

--- angular-medicare-app/src/app/config/my-app-config.ts
+++ angular-medicare-app/src/app/config/my-app-config.ts
@@ -1,2 +1,10 @@
 export default {
+
+    oidc: {
+        clientId: '0oa7r281j2Ux3XbPc5d7',
+        issuer: 'https://dev-48236475.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
 }
