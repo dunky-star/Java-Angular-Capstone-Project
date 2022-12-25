fd3b8f8df14d2ec9f7277d430cb828f904117266
dunky-star <dukygeoffrey39@gmail.com>
2022-12-25 19:11:47 +0300

    Okta OAuth2 config

--- angular-medicare-app/src/app/config/my-app-config.ts
+++ angular-medicare-app/src/app/config/my-app-config.ts
@@ -1,8 +1,8 @@
 export default {
 
     oidc: {
-        clientId: '0oa7r281j2Ux3XbPc5d7',
-        issuer: 'https://dev-48236475.okta.com/oauth2/default',
+        clientId: '0oa7r315tbq8PyHuE5d7',
+        issuer: 'https://dev-57305648.okta.com/oauth2/default',
         redirectUri: 'http://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
