88aea59aa1812e8129e225539f92d7d93a94ac85
dunky-star <dukygeoffrey39@gmail.com>
2022-12-25 21:03:24 +0300

    Handling browser request to not clear Cart items

--- angular-medicare-app/src/app/common/cart-item.ts
+++ angular-medicare-app/src/app/common/cart-item.ts
@@ -2,19 +2,12 @@ import { Drug } from './drug';
 
 export class CartItem {
 
-    id: string;
-    name: string;
-    imageUrl: string;
-    unitPrice: number;
+    constructor(public id?: string,
+                public name?: string,
+                public imageUrl?: string,
+                public unitPrice?: number,
+                public quantity: number = 1) {
 
-    quantity: number;
-
-    constructor(drug: Drug) {
-        this.id = drug.id!;
-        this.name = drug.name!;
-        this.imageUrl = drug.imageUrl!;
-        this.unitPrice = drug.unitPrice!;
-
-        this.quantity = 1;
     }
+
 }
--- angular-medicare-app/src/app/services/cart.service.ts
+++ angular-medicare-app/src/app/services/cart.service.ts
@@ -12,13 +12,29 @@ cartItems: CartItem[] = [];
   totalPrice: Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
-  constructor() { }
+  // To handle broswer refresh that removes items from Cart.
+  storage: Storage = sessionStorage;
+
+  constructor() {
+
+    // read data from storage
+    let data = JSON.parse(this.storage.getItem('cartItems')!);
+
+    if (data != null) {
+      this.cartItems = data;
+
+      // compute totals based on the data that is read from storage
+      this.computeCartTotals();
+    }
+
+  }
 
   addToCart(theCartItem: CartItem) {
 
     // check if we already have the item in our cart
     let alreadyExistsInCart: boolean = false;
-    let existingCartItem: CartItem = undefined!;
+    // let existingCartItem: CartItem = undefined;
+    let existingCartItem: CartItem = new CartItem();
 
     if (this.cartItems.length > 0) {
       // find the item in the cart based on item id
@@ -48,7 +64,7 @@ cartItems: CartItem[] = [];
     let totalQuantityValue: number = 0;
 
     for (let currentCartItem of this.cartItems) {
-      totalPriceValue += currentCartItem.quantity * currentCartItem.unitPrice;
+      totalPriceValue += currentCartItem.quantity * currentCartItem.unitPrice!;
       totalQuantityValue += currentCartItem.quantity;
     }
 
@@ -58,13 +74,20 @@ cartItems: CartItem[] = [];
 
     // log cart data just for debugging purposes
     this.logCartData(totalPriceValue, totalQuantityValue);
+
+    // persist cart data
+    this.persistCartItems();
+  }
+
+  persistCartItems() {
+    this.storage.setItem('cartItems', JSON.stringify(this.cartItems));
   }
 
   logCartData(totalPriceValue: number, totalQuantityValue: number) {
 
     console.log('Contents of the cart');
     for (let tempCartItem of this.cartItems) {
-      const subTotalPrice = tempCartItem.quantity * tempCartItem.unitPrice;
+      const subTotalPrice = tempCartItem.quantity * tempCartItem.unitPrice!;
       console.log(`name: ${tempCartItem.name}, quantity=${tempCartItem.quantity}, unitPrice=${tempCartItem.unitPrice}, subTotalPrice=${subTotalPrice}`);
     }
 
