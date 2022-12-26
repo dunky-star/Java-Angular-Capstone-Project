2e03d336af51c575f629a63bb64d8cb8475fab4d
dunky-star <dukygeoffrey39@gmail.com>
2022-12-26 13:28:47 +0300

    Refactoring - Backend Configs for allowings CORS

--- medicare-app/src/main/java/com/dunky/simplilearn/repositories/DrugRespository.java
+++ medicare-app/src/main/java/com/dunky/simplilearn/repositories/DrugRespository.java
@@ -5,7 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface DrugRespository extends JpaRepository<Drug, Long> {
     Page<Drug> findByCategoryId(@Param("id") Long id, Pageable pageable);
     Page<Drug> findByNameContaining(@Param("name") String name, Pageable pageable);
