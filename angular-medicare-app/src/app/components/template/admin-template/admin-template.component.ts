62bf1efa87c6b00413e4c3ee777fb8dbbccee437
dunky-star <dukygeoffrey39@gmail.com>
2023-01-08 17:00:12 +0300

    Admin template component and view implmentation

--- angular-medicare-app/src/app/components/template/admin-template/admin-template.component.ts
+++ angular-medicare-app/src/app/components/template/admin-template/admin-template.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/common/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-admin-template',
   templateUrl: './admin-template.component.html',
-  styleUrls: ['./admin-template.component.css']
+  styleUrls: ['./admin-template.component.css'],
 })
-export class AdminTemplateComponent {
+export class AdminTemplateComponent implements OnInit {
+  currentUser: User = new User();
 
+  constructor(private userService: UserService, public router: Router) {
+    this.userService.currentUser.subscribe((data) => {
+      this.currentUser = data;
+    });
+  }
+
+  ngOnInit() {}
+
+  logOut() {
+    this.userService.logOut().subscribe(() => {
+      this.router.navigate(['/login']);
+    });
+  }
 }
